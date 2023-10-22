d9e4b8249d94c997a48ce7a4d9d96e3195eb03fe
DESKTOP-K2GASL6\Angel <carmenFGDev@gmail.com>
2023-10-22 12:57:03 +0200

    modify entitiies

--- src/pods/character/api/character.mock-data.ts
+++ src/pods/character/api/character.mock-data.ts
@@ -1,7 +1,7 @@
-import { Hotel } from './hotel.api-model';
+import { Character } from './character.api-model';
 import { Lookup } from 'common/models';
 
-export const mockHotelCollection: Hotel[] = [
+export const mockCharacterCollection: Character[] = [
   {
     id: '1',
     type: 'hotel',
@@ -19,7 +19,7 @@ export const mockHotelCollection: Hotel[] = [
     highRate: 210,
     hotelId: 213497,
     hotelInDestination: true,
-    hotelRating: 4,
+   hotelRating: 4,
     location: {
       latitude: 47.60688,
       longitude: -122.33361,
@@ -45,7 +45,7 @@ export const mockHotelCollection: Hotel[] = [
   {
     id: '2',
     type: 'hotel',
-    name: 'Hotel Monaco Seattle, a Kimpton Hotel',
+    name: 'Character Monaco Seattle, a Kimpton Character',
     created: new Date(1464777092694),
     modified: new Date(1464777092694),
     address1: '1101 4th Ave',
@@ -75,7 +75,7 @@ export const mockHotelCollection: Hotel[] = [
     proximityUnit: 'MI',
     rateCurrencyCode: 'USD',
     shortDescription:
-      'With a stay at Hotel Monaco Seattle, a Kimpton Hotel, you&apos;ll be centrally located in Seattle, steps from Seattle Public Library and Safeco Headquarters. This 4-star',
+      'With a stay at Character Monaco Seattle, a Kimpton Character, you&apos;ll be centrally located in Seattle, steps from Seattle Public Library and Safeco Headquarters. This 4-star',
     stateProvinceCode: 'WA',
     thumbNailUrl: '/thumbnails/62800_122_t.jpg',
     tripAdvisorRating: 4.5,
